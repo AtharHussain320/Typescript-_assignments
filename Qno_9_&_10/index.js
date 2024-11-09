20a024883b18f9cfd243f15590df37f58be18710
AtharHussain320 <atharkeerio66320@gmail.com>
2024-11-09 08:43:22 -0800

    files added

---
+++ Qno_9_&_10/index.js
@@ -0,0 +1,22 @@
+//9.//	Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message
+// that reveals your favorite number. Print that message.
+var mynum = 7;
+var msg = "my favourite number is ".concat(mynum);
+console.log(msg);
+//10.Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have
+// anything specific to write because your programs are too simple at this point, just add your
+// name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Author: [Harry]
+// Date: 2024-11-09
+// This program prints "Hello, World!" to the console.
+function sayHello() {
+    console.log("Hello, World!");
+}
+sayHello();
+// Author: [Harry]
+// Date: 2024-11-09
+// This program calculates the sum of two numbers and prints the result.
+function addNumbers(a, b) {
+    return a + b; // Returns the sum of a and b
+}
+console.log(addNumbers(5, 3));
---
+++ Qno_9_&_10/index.ts
@@ -0,0 +1,42 @@
+//9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message
+// that reveals your favorite number. Print that message.
+
+let mynum = 7
+
+let msg = `my favourite number is ${mynum}`
+
+console.log(msg)
+
+
+
+
+
+
+//10.Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have
+// anything specific to write because your programs are too simple at this point, just add your
+// name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+
+// Author: [Harry]
+// Date: 2024-11-09
+// This program prints "Hello, World!" to the console.
+
+function sayHello(): void {
+    console.log("Hello, World!");
+  }
+  
+  sayHello();
+  
+
+
+// Author: [Harry]
+// Date: 2024-11-09
+// This program calculates the sum of two numbers and prints the result.
+
+function addNumbers(a: number, b: number): number {
+    return a + b; // Returns the sum of a and b
+  }
+  
+  console.log(addNumbers(5, 3));
+  
